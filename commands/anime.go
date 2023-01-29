0974a9ebe34e01f913c5639892d470d4ad247035
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-01-29 14:55:02 +0200

    Add REST command type

--- commands/anime.go
+++ commands/anime.go
@@ -1,13 +1,38 @@
 package commands
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/loghinalexandru/resonator/commands/types"
 )
 
-func NewAnime() *types.Kitsu {
-	out := types.Kitsu{
-		URL: "https://kitsu.io/api/edge/anime?filter[text]=%v&page[limit]=5",
+type content struct {
+	Id    string `json:"id"`
+	Stats struct {
+		Title    string `json:"canonicalTitle"`
+		Type     string `json:"showType"`
+		Rating   string `json:"averageRating"`
+		Rated    string `json:"ageRating"`
+		Status   string `json:"status"`
+		Start    string `json:"startDate"`
+		End      string `json:"endDate"`
+		Episodes int32  `json:"episodeCount"`
+		Length   int32  `json:"episodeLength"`
+		Total    int32  `json:"totalLength"`
+	} `json:"attributes"`
+}
+
+type wrapper struct {
+	Content []content `json:"data"`
+}
+
+func NewAnime() *types.REST {
+	out := types.REST{
+		URL:       "https://kitsu.io/api/edge/anime?filter[text]=%v&page[limit]=5",
+		Type:      &wrapper{},
+		Formatter: kitsuFormat,
 		Def: &discordgo.ApplicationCommand{
 			Name:        "anime",
 			Description: "This command is used find anime via kitsu API!",
@@ -22,4 +47,25 @@ func NewAnime() *types.Kitsu {
 		},
 	}
 	return &out
+}
+
+func kitsuFormat(content any) string {
+	var sb strings.Builder
+	resp, ok := content.(*wrapper)
+
+	if !ok {
+		return "Something went wrong!"
+	}
+
+	if len(resp.Content) > 0 {
+		sb.WriteString("Here's a list: \n")
+		for _, r := range resp.Content {
+			sb.WriteString(fmt.Sprintf("**%s - %s**\n", r.Stats.Title, strings.ToUpper(r.Stats.Status)))
+			sb.WriteString(fmt.Sprintf("> Episodes: %v, Length:  %vm, Total Time: %vm\n", r.Stats.Episodes, r.Stats.Length, r.Stats.Total))
+		}
+	} else {
+		sb.WriteString("No match found :(")
+	}
+
+	return sb.String()
 }
---
+++ commands/types/rest.go
@@ -0,0 +1,53 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type REST struct {
+	URL       string
+	Type      any
+	Formatter func(payload any) string
+	Def       *discordgo.ApplicationCommand
+}
+
+func (cmd *REST) Definition() *discordgo.ApplicationCommand {
+	return cmd.Def
+}
+
+func (cmd *REST) Handler(sess *discordgo.Session, inter *discordgo.InteractionCreate) error {
+	var args []any
+	for _, v := range inter.ApplicationCommandData().Options {
+		args = append(args, v.Value.(string))
+	}
+
+	customURL := fmt.Sprintf(cmd.URL, args...)
+	response, err := http.Get(customURL)
+
+	if err != nil {
+		return nil
+	}
+
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+
+	err = json.Unmarshal(content, cmd.Type)
+
+	if err != nil {
+		return err
+	}
+
+	sess.InteractionRespond(inter.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: cmd.Formatter(cmd.Type),
+		},
+	})
+
+	return nil
+}
