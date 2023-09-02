0f625f326f28177d46fc4356473527aee21ce0f3
Loghin Alexandru <loghinalexandru61@gmail.com>
2023-09-02 12:58:01 +0300

    Use prometheus namespace

--- internal/command/command.go
+++ internal/command/command.go
@@ -46,17 +46,17 @@ func NewManager(ctx *bot.Context) *manager {
 		registered: make([]*discordgo.ApplicationCommand, len(hh)),
 		ctx:        ctx,
 		Request: promauto.NewCounterVec(prometheus.CounterOpts{
-			Subsystem: "resonator",
+			Namespace: "resonator",
 			Name:      "command_requests_total",
 			Help:      "The total number of commands invoked",
 		}, []string{"command"}),
 		Error: promauto.NewCounterVec(prometheus.CounterOpts{
-			Subsystem: "resonator",
+			Namespace: "resonator",
 			Name:      "command_errors_total",
 			Help:      "The total number of commands errors",
 		}, []string{"command"}),
 		Duration: promauto.NewHistogramVec(prometheus.HistogramOpts{
-			Subsystem: "resonator",
+			Namespace: "resonator",
 			Name:      "command_duration_seconds",
 			Help:      "The duration of a command invocation",
 			Buckets:   []float64{.1, .25, .5, 1, 2.5, 5, 10},
